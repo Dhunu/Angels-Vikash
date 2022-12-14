e8bc590d067cf689e3220c795724e9eef67e5763
Dhunu <angelsaikia333@gmail.com>
2022-12-14 22:09:08 +0530

    day-2

---
+++ Angels_Vikash/src/com/angel/usecases/CreateEmployee.java
@@ -0,0 +1,5 @@
+package com.angel.usecases;
+
+public class CreateEmployee {
+
+}
---
+++ Angels_Vikash/src/com/angel/utility/DBUtils.java
@@ -0,0 +1,17 @@
+package com.angel.utility;
+
+import java.sql.Connection;
+
+public class DBUtils {
+	
+	public static void main(String[] args) {
+		
+		try() {
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+	}
+
+}
